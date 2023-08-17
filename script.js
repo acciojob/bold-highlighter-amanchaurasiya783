23d349b7a3f62ca9cfbdad68b74b3bf3357645d0
amanchaurasiya783 <127719170+amanchaurasiya783@users.noreply.github.com>
2023-08-17 09:32:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function highlight() {
     //Write your code here
-
+	
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	let strong = document.getElementsByTagName('strong');
+	strong.forEach((element) =>{
+		element.ClassList.addClass = 'green';
+	})
     
 }
